43e7462efe57d2e6574cd0ebebb31a6a37f316d2
rolaeru <olaeru.razvan@gmail.com>
2018-02-21 11:55:46 +0200

    spring DI implementation

--- src/main/java/com/pentalog/razvan/spring5didemo/Spring5DiDemoApplication.java
+++ src/main/java/com/pentalog/razvan/spring5didemo/Spring5DiDemoApplication.java
@@ -1,6 +1,9 @@
 package com.pentalog.razvan.spring5didemo;
 
+import com.pentalog.razvan.spring5didemo.controllers.ConstructorInjectedController;
 import com.pentalog.razvan.spring5didemo.controllers.MyController;
+import com.pentalog.razvan.spring5didemo.controllers.PropertyInjectedController;
+import com.pentalog.razvan.spring5didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,5 +17,9 @@ public class Spring5DiDemoApplication {
 		MyController controller = (MyController) ctx.getBean("myController");
 
 		controller.hello();
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
--- src/main/java/com/pentalog/razvan/spring5didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/pentalog/razvan/spring5didemo/controllers/PropertyInjectedController.java
@@ -2,9 +2,13 @@ package com.pentalog.razvan.spring5didemo.controllers;
 
 import com.pentalog.razvan.spring5didemo.services.GreetingService;
 import com.pentalog.razvan.spring5didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingServiceImpl greetingService;
 
     public String sayHello() {
